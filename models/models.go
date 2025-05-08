dfb55466b24ba36a05e1613dbcd4447ce7d95669
Сава <sava_nedno@mail.ru>
2025-05-08 03:41:09 +0300

    update structures

--- models/models.go
+++ models/models.go
@@ -11,5 +11,5 @@ type User struct {
 	ID           uint   `gorm:"primaryKey;autoIncrement" json:"id"`
 	Username     string `gorm:"unique;not null"      json:"username"`
 	PasswordHash string `gorm:"not null"             json:"-"`
-	Role         string `gorm:"not null;default:user" json:"role"`
+	Role         string `gorm:"not null;" json:"role"`
 }
