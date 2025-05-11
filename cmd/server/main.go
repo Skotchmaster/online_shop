b35c7b6293635a8edef90a2e66b0307aed08d7d9
Сава <sava_nedno@mail.ru>
2025-05-11 22:13:22 +0300

    create jwt token

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,25 +2,36 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
-	"github.com/Skotchmaster/project_for_t_bank/internal/handlers"
+	"github.com/Skotchmaster/online_shop/internal/handlers"
+	"github.com/Skotchmaster/online_shop/internal/jwtmiddleware"
 )
 
 func main() {
-	db, err := handlers.initDB()
+	db, err := handlers.InitDB()
 	if err != nil {
 		log.Fatalf("Ошибка инициализации БД: %v", err)
 	}
 
-	app := &handlers.App{DB: db}
+	accessSecret := []byte(os.Getenv("ACCESS_SECRET"))
+	refreshSecret := []byte(os.Getenv("REFRESH_SECRET"))
+
+	app := &handlers.ProductHandler{DB: db}
+	authHandler := &handlers.AuthHandler{DB: db, JWTSecret: accessSecret, RefreshSecret: refreshSecret}
 	e := echo.New()
-	msgs := e.Group("/messages")
-	msgs.GET("", app.GetHandler)
-	msgs.POST("", app.PostHandler)
-	msgs.PATCH("/:id", app.PatchHandler)
-	msgs.DELETE("/:id", app.DeleteHandler)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.POST("/register", authHandler.Register)
+	e.POST("/login", authHandler.Login)
+
+	api := e.Group("/product", jwtmiddleware.JWTMiddleware(accessSecret))
+
+	api.POST("", app.CreateProduct)
 
 	e.Logger.Fatal(e.Start(":8080"))
 
---
+++ internal/jwtmiddleware/jwt.go
@@ -0,0 +1,15 @@
+package jwtmiddleware
+
+import (
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+)
+
+func JWTMiddleware(secret []byte) echo.MiddlewareFunc {
+	cfg := echojwt.WithConfig(echojwt.Config{
+		SigningKey:  secret,
+		ContextKey:  "user",
+		TokenLookup: "header:Authorization: Bearer",
+	})
+	return cfg
+}
