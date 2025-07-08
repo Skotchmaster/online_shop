afe7d285065281a5ab2e3198cd2cdcb31886e95e
Skotchmaster <sava_nedno@mail.ru>
2025-07-08 04:04:24 +0300

    made jwt-midddleware logic simplier

--- internal/jwtmiddleware/jwt.go
+++ internal/jwtmiddleware/jwt.go
@@ -1,29 +1,15 @@
 package jwtmiddleware
 
 import (
-	"fmt"
-
-	"github.com/golang-jwt/jwt/v5"
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
-func JWTMiddleware(keyStore map[string][]byte) echo.MiddlewareFunc {
-	cfg := echojwt.WithConfig(echojwt.Config{
+func JWTMiddleware(secret []byte) echo.MiddlewareFunc {
+	return echojwt.WithConfig(echojwt.Config{
 		SigningMethod: "HS256",
+		SigningKey:    secret,
 		ContextKey:    "user",
 		TokenLookup:   "header:Authorization",
-		KeyFunc: func(token *jwt.Token) (interface{}, error) {
-			kidVal, ok := token.Header["kid"].(string)
-			if !ok {
-				return nil, fmt.Errorf("JWT token missing kid header")
-			}
-			secret, exists := keyStore[kidVal]
-			if !exists {
-				return nil, fmt.Errorf("unknown kid %s", kidVal)
-			}
-			return secret, nil
-		},
 	})
-	return cfg
 }
